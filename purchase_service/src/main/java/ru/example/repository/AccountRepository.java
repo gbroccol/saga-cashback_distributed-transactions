9ea9781ea6942a6c293a4f0f780c36569e613346
pchelina.a2 <pchelina.a2@wildberries.work>
2025-01-16 00:11:45 +0100

    del

--- purchase_service/src/main/java/ru/example/repository/AccountRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.example.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.example.model.Account;
-
-public interface AccountRepository extends JpaRepository<Account, Long> {
-}
