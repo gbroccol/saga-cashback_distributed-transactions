b2fa9b665744707daccffbfdcc6ed5e2b5188bea
pchelina.a2 <pchelina.a2@wildberries.work>
2025-01-16 00:14:03 +0100

    del

--- purchase_service/src/main/java/ru/example/service/AccountCreation.java
+++
@@ -1,8 +0,0 @@
-package ru.example.service;
-
-import ru.example.model.AccountRequest;
-import ru.example.model.AccountResponse;
-
-public interface AccountCreation {
-    AccountResponse create(AccountRequest accountRequest);
-}
