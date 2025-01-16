86f178e9d6914720aa3ccdc9dce873308717376c
pchelina.a2 <pchelina.a2@wildberries.work>
2025-01-16 17:26:33 +0100

    ApplicationConfig

--- purchase_service/src/main/java/ru/example/purchase/config/ApplicationConfig.java
+++ purchase_service/src/main/java/ru/example/purchase/config/ApplicationConfig.java
@@ -1,14 +1,12 @@
-package ru.example.config;
+package ru.example.purchase.config;
 
-import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.KafkaTemplate;
-import ru.example.sender.DataSender;
-import ru.example.sender.DataSenderKafka;
+import ru.example.purchase.sender.DataSender;
+import ru.example.purchase.sender.DataSenderKafka;
 
 
 @Configuration
@@ -22,22 +20,6 @@ public class ApplicationConfig {
                              @Value("${application.kafka.topic.purchase-canceling:purchase_canceling}") String topicPurchaseCanceling) {
         this.topicPurchaseCreating = topicPurchaseCreating;
         this.topicPurchaseCanceling = topicPurchaseCanceling;
-    }
-
-    @Bean
-    NewTopic purchaseCreatedTopic() {
-        return TopicBuilder.name(topicPurchaseCreating)
-                .partitions(2)
-                .replicas(2)
-                .build();
-    }
-
-    @Bean
-    NewTopic purchaseCanceledTopic() {
-        return TopicBuilder.name(topicPurchaseCanceling)
-                .partitions(2)
-                .replicas(2)
-                .build();
     }
 
     @Bean
