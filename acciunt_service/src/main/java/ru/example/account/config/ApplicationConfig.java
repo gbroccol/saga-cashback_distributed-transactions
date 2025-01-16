ddc2c181dffb994605f83fe13052990e464a5e77
pchelina.a2 <pchelina.a2@wildberries.work>
2025-01-16 17:26:29 +0100

    ApplicationConfig

--- acciunt_service/src/main/java/ru/example/account/config/ApplicationConfig.java
+++ acciunt_service/src/main/java/ru/example/account/config/ApplicationConfig.java
@@ -1,11 +1,9 @@
 package ru.example.account.config;
 
-import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.KafkaTemplate;
 import ru.example.account.sender.DataSender;
 import ru.example.account.sender.DataSenderKafka;
@@ -24,30 +22,6 @@ public class ApplicationConfig {
         this.topicPurchaseCreated = topicPurchaseCreated;
         this.topicPurchaseRejected = topicPurchaseRejected;
         this.topicPurchaseCanceled = topicPurchaseCanceled;
-    }
-
-    @Bean
-    NewTopic purchaseCreatedTopic() {
-        return TopicBuilder.name(topicPurchaseCreated)
-                .partitions(2)
-                .replicas(2)
-                .build();
-    }
-
-    @Bean
-    NewTopic purchaseRejectedTopic() {
-        return TopicBuilder.name(topicPurchaseRejected)
-                .partitions(2)
-                .replicas(2)
-                .build();
-    }
-
-    @Bean
-    NewTopic purchaseCanceledTopic() {
-        return TopicBuilder.name(topicPurchaseCanceled)
-                .partitions(2)
-                .replicas(2)
-                .build();
     }
 
     @Bean
