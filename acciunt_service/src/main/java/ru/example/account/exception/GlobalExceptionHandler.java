d07f3678285d2a33aea998932bf8de13a7d412b4
pchelina.a2 <pchelina.a2@wildberries.work>
2025-01-16 17:27:13 +0100

    ex

---
+++ acciunt_service/src/main/java/ru/example/account/exception/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package ru.example.account.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(AccountDoesNotExistException.class)
+    public ResponseEntity<ErrorMessage> exception(AccountDoesNotExistException e) {
+        return new ResponseEntity<>(new ErrorMessage(e.getMessage()), HttpStatus.BAD_REQUEST);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(NotEnoughMoneyException.class)
+    public ResponseEntity<ErrorMessage> exception(NotEnoughMoneyException e) {
+        return new ResponseEntity<>(new ErrorMessage(e.getMessage()), HttpStatus.BAD_REQUEST);
+    }
+
+}
